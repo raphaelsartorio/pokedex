09d63838afb0cf3c7a7b0b1bd96050a63a63c25a
Raphael Sartorio <raphaelsartorio9@gmail.com>
2021-03-04 08:21:35 -0300

    Console

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,6 @@ export default {
   },
   created: function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
-      console.log("Pegou a lista do pokemon");
       this.pokemons = res.data.results;
       this.filteredPokemon = res.data.results;
     })
